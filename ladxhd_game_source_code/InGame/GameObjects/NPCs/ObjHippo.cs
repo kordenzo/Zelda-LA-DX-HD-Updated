ac66b5d7be8589308735ba0ee62ef5bf6002257b
Scott Zimmerman <bighead.0@gmail.com>
2025-09-04 11:41:36 -0400

    Implement Hippo standing state and AI to transition to sitting when getting close.

--- ladxhd_game_source_code/InGame/GameObjects/NPCs/ObjHippo.cs
+++ ladxhd_game_source_code/InGame/GameObjects/NPCs/ObjHippo.cs
@@ -3,6 +3,7 @@ using ProjectZ.Base;
 using ProjectZ.InGame.GameObjects.Base;
 using ProjectZ.InGame.GameObjects.Base.CObjects;
 using ProjectZ.InGame.GameObjects.Base.Components;
+using ProjectZ.InGame.GameObjects.Base.Components.AI;
 using ProjectZ.InGame.Map;
 using ProjectZ.InGame.SaveLoad;
 using ProjectZ.InGame.Things;
@@ -12,7 +13,7 @@ namespace ProjectZ.InGame.GameObjects.NPCs
     class ObjHippo : GameObject
     {
         private readonly Animator _animator;
-
+        private readonly AiComponent _aiComponent;
         private int _direction = -1;
 
         public ObjHippo() : base("hippo") { }
@@ -20,33 +21,47 @@ namespace ProjectZ.InGame.GameObjects.NPCs
         public ObjHippo(Map.Map map, int posX, int posY) : base(map)
         {
             EntityPosition = new CPosition(posX + 8, posY + 16, 0);
-            EntitySize = new Rectangle(-12, -24, 24, 24);
+            EntitySize = new Rectangle(-12, -24, 24, 32);
 
             _animator = AnimatorSaveLoad.LoadAnimator("NPCs/npc_hippo");
-            _animator.Play("idle_" + _direction);
+            _animator.Play("stand_" + _direction);
 
             var sprite = new CSprite(EntityPosition);
             var animationComponent = new AnimationComponent(_animator, sprite, Vector2.Zero);
 
             var body = new BodyComponent(EntityPosition, -9, -13, 18, 12, 8);
 
+            _aiComponent = new AiComponent();
+            var stateStand = new AiState(UpdateStanding);
+            var stateEmbarassed = new AiState(UpdateEmbarassed);
+
+            _aiComponent.States.Add("stand", stateStand);
+            _aiComponent.States.Add("embarassed", stateEmbarassed);
+            _aiComponent.ChangeState("stand");
+
+            AddComponent(AiComponent.Index, _aiComponent);
             AddComponent(BodyComponent.Index, body);
             AddComponent(CollisionComponent.Index, new BodyCollisionComponent(body, Values.CollisionTypes.Normal | Values.CollisionTypes.PushIgnore));
             AddComponent(InteractComponent.Index, new InteractComponent(new CBox(EntityPosition, -9, -5, 18, 5, 8), Interact));
             AddComponent(BaseAnimationComponent.Index, animationComponent);
-            AddComponent(UpdateComponent.Index, new UpdateComponent(Update));
             AddComponent(DrawComponent.Index, new BodyDrawComponent(body, sprite, Values.LayerPlayer));
             AddComponent(DrawShadowComponent.Index, new BodyDrawShadowComponent(body, sprite));
         }
 
-        private void Update()
+        private void UpdateStanding()
         {
-            var box = new RectangleF(EntityPosition.X + _direction * 14 - 4, EntityPosition.Y - 14, 8, 18);
-            if (MapManager.ObjLink.BodyRectangle.Intersects(box))
-            {
+            var box_bashful = new RectangleF(EntityPosition.X - 16, EntityPosition.Y + 12, 64, 16);
+            if (MapManager.ObjLink.BodyRectangle.Intersects(box_bashful))
+                _aiComponent.ChangeState("embarassed");
+        }
+
+        private void UpdateEmbarassed()
+        {
+            var box_turnAround = new RectangleF(EntityPosition.X + _direction * 14 - 4, EntityPosition.Y - 14, 8, 18);
+            if (MapManager.ObjLink.BodyRectangle.Intersects(box_turnAround))
                 _direction = -_direction;
-                _animator.Play("idle_" + _direction);
-            }
+
+            _animator.Play("idle_" + _direction);
         }
 
         private bool Interact()
