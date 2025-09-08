c56d11f66c21b653224658bd5cb0b3682eda302c
Scott Zimmerman <bighead.0@gmail.com>
2025-09-07 20:16:55 -0400

    Didn't need that include. VS adding stuff automatically is great but sometimes annoying.

--- ladxhd_game_source_code/InGame/GameObjects/Things/ObjMoveStone.cs
+++ ladxhd_game_source_code/InGame/GameObjects/Things/ObjMoveStone.cs
@@ -9,7 +9,6 @@ using ProjectZ.InGame.GameObjects.Base.Components.AI;
 using ProjectZ.InGame.GameObjects.Base.Systems;
 using ProjectZ.InGame.Map;
 using ProjectZ.InGame.Things;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace ProjectZ.InGame.GameObjects.Things
 {
