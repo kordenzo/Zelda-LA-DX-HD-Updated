ee0278a6aff26f67afd763dd591e901b7c778236
Scott Zimmerman <bighead.0@gmail.com>
2025-10-18 15:59:40 -0400

    Create "ModFile.cs" with "ModFile" class to reduce code bloat.
    Any object that can parse the standard "lahdmod" file has this method in it, so instead of copy and pasting it over and over may as well have a centralized location for it. This "Parse" method should work for the majority of current lahdmod files and a few that haven't been released yet.

---
+++ ladxhd_game_source_code/InGame/Things/ModFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Reflection;
+using ProjectZ.InGame.GameObjects.Base;
+
+namespace ProjectZ.InGame.Things
+{
+    public static class ModFile
+    {
+        public static void Parse(string modFile, GameObject gameObject)
+        {
+            foreach (string line in File.ReadAllLines(modFile))
+            {
+                if (string.IsNullOrWhiteSpace(line) || line.StartsWith("//"))
+                    continue;
+
+                string[] splitLine = line.Split(new char[]{ '=', '/' });
+                if (splitLine.Length < 2)
+                    continue;
+
+                string varName = splitLine[0].Trim();
+                string varValue = splitLine[1].Trim();
+
+                FieldInfo field = gameObject.GetType().GetField(varName, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+                object convertedValue = Convert.ChangeType(varValue, field.FieldType, CultureInfo.InvariantCulture);
+                field.SetValue(gameObject, convertedValue);
+            }
+        }
+    }
+}
