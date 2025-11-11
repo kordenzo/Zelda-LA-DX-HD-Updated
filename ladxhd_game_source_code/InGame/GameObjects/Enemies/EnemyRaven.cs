b4e77f2df64799315efee69f0ed9599df07f4dbc
Scott Zimmerman <bighead.0@gmail.com>
2025-11-11 12:43:45 -0500

    Remove enemy Raven from "Always Animate" objects list.
    They are causing problems I don't know how to solve. Their position at the top left of the overworld map causes issues during map transitions. I think it's because Link's position is temporarily set to 0,0 between transitions, so it's triggering the Ravens. I tried adding a delay which does work on a single transition, but if quickly transitioning back and forth between maps they still end up triggering. Until I can come up with a better solution, they can not be always animated.

--- ladxhd_game_source_code/InGame/GameObjects/Enemies/EnemyRaven.cs
+++ ladxhd_game_source_code/InGame/GameObjects/Enemies/EnemyRaven.cs
@@ -84,7 +84,6 @@ namespace ProjectZ.InGame.GameObjects.Enemies
             AddComponent(DrawShadowComponent.Index, new BodyDrawShadowComponent(_body, _sprite));
 
             new ObjSpriteShadow("sprshadowm", this, Values.LayerPlayer, map);
-            ObjectManager.AlwaysAnimateObjectsMain.Add(this);
         }
 
         private void UpdateIdle()
