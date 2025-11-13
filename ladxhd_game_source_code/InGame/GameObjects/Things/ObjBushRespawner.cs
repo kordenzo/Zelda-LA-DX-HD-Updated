efe7f99a5704108b79dfc3a78ed4feb881f6d7c2
Scott Zimmerman <bighead.0@gmail.com>
2025-11-13 13:09:05 -0500

    Classic Camera: Respawn bushes/grass on a field change.
    The original game respawned shrubbery each time the screen scrolled, so let's do that here too. This uses the "FieldChange" variable on "ObjLink" to know when to respawn the grass/bush objects. There is a slight delay so the respawn isn't visible until after the camera pans, but it's probably still possible to view it if the player is quick enough.

--- ladxhd_game_source_code/InGame/GameObjects/Things/ObjBushRespawner.cs
+++ ladxhd_game_source_code/InGame/GameObjects/Things/ObjBushRespawner.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using ProjectZ.InGame.GameObjects.Base;
 using ProjectZ.InGame.GameObjects.Base.CObjects;
 using ProjectZ.InGame.GameObjects.Base.Components;
+using ProjectZ.InGame.Map;
 
 namespace ProjectZ.InGame.GameObjects.Things
 {
@@ -16,6 +17,9 @@ namespace ProjectZ.InGame.GameObjects.Things
         private readonly string _pickupKey;
 
         private int _lastFieldTime;
+
+        private bool _respawnStart;
+        private float _respawnTimer;
 
         public ObjBushRespawner(Map.Map map, int posX, int posY, string spawnItem, string spriteId,
             bool hasCollider, bool drawShadow, bool setGrassField, int drawLayer, string pickupKey) : base(map)
@@ -35,20 +39,44 @@ namespace ProjectZ.InGame.GameObjects.Things
 
             // add key change listener
             AddComponent(UpdateComponent.Index, new UpdateComponent(Update));
+            Map.Objects.RegisterAlwaysAnimateObject(this);
         }
 
         private void Update()
         {
-            // field went out of the update range?
-            var updateState = Map.GetUpdateState(EntityPosition.Position);
-            if (_lastFieldTime < updateState)
-                SpawnObject();
+            // If classic camera mode is active, respawn when leaving the current field.
+            if (Camera.ClassicMode)
+            {
+                if (MapManager.ObjLink.FieldChange)
+                    _respawnStart = true;
+
+                // Respawn after a slight delay. Always animate list makes sure that all
+                // of the bushes and grass respawn even when they are off the screen.
+                if (_respawnStart)
+                {
+                    _respawnTimer += Game1.DeltaTime;
+                    if (_respawnTimer >= 350)
+                    {
+                        SpawnObject();
+                        _respawnTimer = 0;
+                        _respawnStart = false;
+                    }
+                }
+            }
+            // If the field is outside of the update range. Link must be at least 3
+            // fields away from the field the shrub is found on for it to respawn.
+            else
+            {
+                var updateState = Map.GetUpdateState(EntityPosition.Position);
+                if (_lastFieldTime < updateState)
+                    SpawnObject();
+            }
         }
 
         private void SpawnObject()
         {
+            // Delete the respawn object and respawn the bush or grass object.
             Map.Objects.DeleteObjects.Add(this);
-
             Map.Objects.SpawnObject(new ObjBush(Map, (int)EntityPosition.X, (int)EntityPosition.Y,
                 _spawnItem, _spriteId, _hasCollider, _drawShadow, _setGrassField, _drawLayer, _pickupKey));
         }
