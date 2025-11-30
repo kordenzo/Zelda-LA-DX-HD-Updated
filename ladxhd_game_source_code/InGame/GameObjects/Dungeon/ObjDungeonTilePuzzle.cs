c4fd7b333b4eb9388c47995555e76e99fe6d7d03
Scott Zimmerman <bighead.0@gmail.com>
2025-11-30 07:18:08 -0500

    Add an additional dungeon 4 tile-order puzzle.
    After browsing tons of YouTube videos I managed to find another tile puzzle order so I added it to the array.

--- ladxhd_game_source_code/InGame/GameObjects/Dungeon/ObjDungeonTilePuzzle.cs
+++ ladxhd_game_source_code/InGame/GameObjects/Dungeon/ObjDungeonTilePuzzle.cs
@@ -17,7 +17,7 @@ namespace ProjectZ.InGame.GameObjects.Dungeon
         {
             // If the puzzle key has not yet been set, create a new puzzle key.
             if (Game1.GameManager.SaveManager.GetString(puzzleKey, "0") == "0")
-                Game1.GameManager.SaveManager.SetString(puzzleKey, Game1.RandomNumber.Next(1, 4).ToString());
+                Game1.GameManager.SaveManager.SetString(puzzleKey, Game1.RandomNumber.Next(1, 5).ToString());
 
             // Load whatever the puzzle key has been set to.
             var variation = Game1.GameManager.SaveManager.GetString(puzzleKey);
@@ -25,9 +25,11 @@ namespace ProjectZ.InGame.GameObjects.Dungeon
             // The puzzle key determines the button order that must be stepped on.
             var stepOrder = variation switch
             {
+                // Tile order: Top-Left, Top Right, Middle, Bottom-Left, Bottom-Right
                 "1" => new int[] { 3, 2, 0, 1, 4 },
                 "2" => new int[] { 0, 4, 2, 3, 1 },
-                "3" => new int[] { 2, 3, 4, 0, 1 }
+                "3" => new int[] { 2, 3, 4, 0, 1 },
+                "4" => new int[] { 4, 1, 0, 3, 2 }
             };
             // The positions of the tiles. Puzzle tile should be placed on center tile.
             Point[] tilePos = new Point[5];
